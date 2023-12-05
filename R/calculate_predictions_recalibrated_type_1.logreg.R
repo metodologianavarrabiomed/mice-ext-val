cdf9f3630ba2e31a7220ba2ec38bcb4afb290a6b
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2023-12-05 12:45:17 +0100

    :sparkles: Add calcualtion of type 1 recalibration in logreg

--- R/calculate_predictions_recalibrated_type_1.logreg.R
+++ R/calculate_predictions_recalibrated_type_1.logreg.R
@@ -13,8 +13,57 @@
 #'
 #' @examples
 #' model %>%
-#'    calculate_predictions(data) |>
-#'    calculate_predictions_recalibrated_type_1(data)
+#'   calculate_predictions(data) |>
+#'   calculate_predictions_recalibrated_type_1(data)
 calculate_predictions_recalibrated_type_1.logreg <- function(model, data, .progress = TRUE) {
+  # Checks pre-conditions
+  stopifnot(is(model, "MiceExtVal"))
+  stopifnot(is(data, "data.frame"))
 
+  # Progress bar code
+  if (.progress) {
+    n_iter <- max(data$.imp) + 1
+    pb <- progress::progress_bar$new(
+      format = "Type 1 recalibration \t[:bar] :percent [E.T.: :elapsedfull || R.T.: :eta]",
+      total = n_iter,
+      complete = "=",
+      incomplete = "-",
+      current = ">",
+      clear = FALSE,
+      width = 100
+    )
+  }
+
+  model$alpha_type_1 <- data %>%
+    dplyr::group_by(.imp) %>%
+    dplyr::group_map(~ {
+      # Progress bar code
+      if (.progress) {
+        pb$tick()
+      }
+
+      # Obtains the data of the event variable
+      survival_data <- .x[[all.vars(model$formula)[1]]]
+      betax <- model$betax_data %>%
+        dplyr::filter(.imp == .y$.imp) %>%
+        dplyr::select(betax) %>%
+        unlist()
+
+      # Calculates the `alpha` parameter value
+      model_recal <- rms::lrm.fit(
+        y = survival_data[, "status"],
+        offset = betax
+      )
+      alpha <- model_recal$coefficients
+    }) %>%
+    do.call(rbind, args = .) %>%
+    mean()
+
+  # Calculates the type 1 recalibration
+  model$predictions_recal_type_1 <- tibble::tibble(
+    id = model$betax$id,
+    prediction_type_1 = 1 / (1 + exp(-(model$betax$betax + model$alpha_type_1)))
+  )
+
+  return(model)
 }
