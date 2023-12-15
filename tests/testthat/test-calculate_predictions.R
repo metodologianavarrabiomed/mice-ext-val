43ec8e1079d6c4ff6c8fedb479d499c967492c15
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2023-12-15 08:55:35 +0100

    :test_tube: Add test calculate predictions in `logreg` model

--- tests/testthat/test-calculate_predictions.R
+++ tests/testthat/test-calculate_predictions.R
@@ -4,7 +4,6 @@
 source(test_path("fixtures", "make-model-cox.R"))
 source(test_path("fixtures", "make-model-logreg.R"))
 
-
 test_that("Checks the model parameter", {
   expect_error(calculate_predictions())
   expect_error(calculate_predictions(5))
@@ -24,10 +23,9 @@ test_that("Checks the data argument in logreg model", {
 })
 
 test_that("Calculates the predictions properly in Cox model", {
-  model <- make_cox_model(environment())
   data <- readRDS(test_path("fixtures", "mice_data.rds"))
 
-  model <- model |>
+  model <- make_cox_model(environment()) |>
     calculate_predictions(data)
 
   # Predictions works
@@ -38,5 +36,22 @@ test_that("Calculates the predictions properly in Cox model", {
   betax <- readRDS(test_path("fixtures", "cox", "betax_cox.rds"))
   expect_identical(model$betax, betax)
   betax_data <- readRDS(test_path("fixtures", "cox", "betax_data_cox.rds"))
+  expect_identical(model$betax_data, betax_data)
+})
+
+test_that("Calculates the predictions properly in logreg model", {
+  data <- readRDS(test_path("fixtures", "mice_data.rds"))
+
+  model <- make_logreg_model(environment()) |>
+    calculate_predictions(data)
+
+  # Predictions works
+  predictions_aggregated <- readRDS(test_path("fixtures", "logreg", "predictions_aggregated_logreg.rds"))
+  expect_identical(model$predictions_aggregated, predictions_aggregated)
+  predictions_data <- readRDS(test_path("fixtures", "logreg", "predictions_data_logreg.rds"))
+  expect_identical(model$predictions_data, predictions_data)
+  betax <- readRDS(test_path("fixtures", "logreg", "betax_logreg.rds"))
+  expect_identical(model$betax, betax)
+  betax_data <- readRDS(test_path("fixtures", "logreg", "betax_data_logreg.rds"))
   expect_identical(model$betax_data, betax_data)
 })
