8d79cae51e4a1cb8e70cbde23425669414deb0a3
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2023-12-21 09:03:50 +0100

    :art: Fix warning in S3 methods and their parameters. Now generic methods have the same parameters as the S3 analogous methods

--- R/calculate_predictions_recalibrated_type_1.R
+++ R/calculate_predictions_recalibrated_type_1.R
@@ -5,7 +5,8 @@
 #' A generic method for calculating the type 1 recalibration predictions for a model.
 #'
 #' @param model Model for which the recalibrated predictions are calculated
-#' @param ... Parameters for [calculate_predictions_recalibrated_type_1.cox()] function or for [calculate_predictions_recalibrated_type_1.logreg()] function.
+#' @param data Data parameter for [calculate_predictions_recalibrated_type_1.cox()] function or for [calculate_predictions_recalibrated_type_1.logreg()] function.
+#' @param .progress .progress parameter for [calculate_predictions_recalibrated_type_1.cox()] function or for [calculate_predictions_recalibrated_type_1.logreg()] function.
 #'
 #' @return A model with the parameter `predictions_recal_type_1` and also the recalibration parameters are populated.
 #'
@@ -35,7 +36,7 @@
 #' model |>
 #'   calculate_predictions(data) |>
 #'   calculate_predictions_recalibrated_type_1(data)
-calculate_predictions_recalibrated_type_1 <- function(model, ...){
+calculate_predictions_recalibrated_type_1 <- function(model, data, .progress){
   stopifnot(methods::is(model, "MiceExtVal"))
 
   UseMethod("calculate_predictions_recalibrated_type_1", model)
--- R/calculate_predictions_recalibrated_type_2.R
+++ R/calculate_predictions_recalibrated_type_2.R
@@ -5,7 +5,8 @@
 #' A generic method for calculating the type 2 recalibration predictions for the given model.
 #'
 #' @param model Model for which the recalibrated predictions are calculated
-#' @param ... Parameters for [calculate_predictions_recalibrated_type_2.cox()] function or [calculate_predictions_recalibrated_type_2.logreg()] function
+#' @param data Data parameter for [calculate_predictions_recalibrated_type_2.cox()] function or [calculate_predictions_recalibrated_type_2.logreg()] function
+#' @param .progress .progress parameter for [calculate_predictions_recalibrated_type_2.cox()] function or [calculate_predictions_recalibrated_type_2.logreg()] function
 #'
 #' @return A model with the parameter `predictions_recal_type_2` and also the recalibration parameters are populated.
 #'
@@ -36,7 +37,7 @@
 #'   calculate_predictions(data) |>
 #'   calculate_predictions_recalibrated_type_1(data) |>
 #'   calculate_predictions_recalibrated_type_2(data)
-calculate_predictions_recalibrated_type_2 <- function(model, ...) {
+calculate_predictions_recalibrated_type_2 <- function(model, data, .progress) {
   stopifnot(methods::is(model, "MiceExtVal"))
 
   UseMethod("calculate_predictions_recalibrated_type_2", model)
