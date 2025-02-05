d569417e5e77e5ac9ba6f1983f4321760d46d7d2
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2025-02-05 14:47:25 +0100

    extract the error treatment in recalibrated type 1 to a function

---
+++ R/get_error_message_calculate_type_1.R
@@ -0,0 +1,47 @@
+#' generates the error messages for the `calculate_predictions_recalibrated_type_1`
+#'
+#' @param model
+#' @param data
+#'
+#' @returns the error message for the given model and data
+get_error_message_calculate_type_1 <- function(model, data) {
+  error_message <- NULL
+  # check that the dataset is multiple imputed and contains an id
+  if (!".imp" %in% colnames(data)) {
+    error_message <- c(error_message, "*" = cli::format_error("The variable {.var .imp} (number of imputation each row is part of) must be present in {.arg data}"))
+  }
+
+  if (!"id" %in% colnames(data)) {
+    error_message <- c(error_message, "*" = cli::format_error("The variable {.var id} (unique identifier for each row) must be present in {.arg data}"))
+  }
+
+  # Returns an error if `predictions_data` does not exist in `model`
+  if (!"predictions_data" %in% names(model) | !methods::is(model$predictions_data, "data.frame")) {
+    error_message <- c(error_message, "*" = cli::format_error("In {.arg model} there should be the argument {.arg predictions_data} {.cls tibble} calculated (see {.fn MiceExtVal::calculate_predictions})"))
+  }
+
+  # Returns an error if the dependent variable in the model formula does not exist
+  # in `data` or is not a survival class
+  dependent_variable <- all.vars(model$formula)[1]
+  if (!dependent_variable %in% colnames(data)) {
+    error_message <- c(error_message, "*" = cli::format_error("The dependent variable {.var {dependent_variable}} must be part of {.arg data}"))
+  }
+  if (!methods::is(data[[dependent_variable]], "Surv")) {
+    error_message <- c(error_message, "*" = cli::format_error("The dependent variable {.var {dependent_variable}} must be {.cls Surv}"))
+  }
+
+  if (methods::is(model, "cox")) {
+    # Returns an error if `S0` does not exist or it is bad defined in the cox model
+    if (is.null(model$S0) | !is.numeric(model$S0)) {
+      error_message <- c(error_message, "*" = cli::format_error("{.arg S0} must be {.cls numeric}"))
+    }
+  }
+
+  if (methods::is(model, "logreg")) {
+    if (is.null(model$intercept) | !is.numeric(model$intercept)) {
+      error_message <- c(error_message, "*" = cli::format_error("{.arg intercept} must be {.cls numeric}"))
+    }
+  }
+
+  return(error_message)
+}
