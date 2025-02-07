61c5fc23b6da18827e623519d84b00ee3cd8c45a
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2025-02-07 14:06:17 +0100

    update error messages to the new models and information inside them

--- R/get_error_message_calculate.R
+++ R/get_error_message_calculate.R
@@ -9,28 +9,11 @@
 get_error_message_calculate <- function(model, data) {
   error_message <- NULL
 
-  # check that the coefficients variables are in the `data` argument
-  if (is.null(model$coefficients)) {
-    error_message <- c(error_message, cli::format_error(""))
+  if (!is.null(model$formula) & !all(all.vars(model$formula) %in% colnames(data))) {
+    variables_not_present <- all.vars(model$formula) %in% colnames(data)
+    variables <- all.vars(model$formula)[!variables_not_present]
+    error_message <- c(error_message, "*" = cli::format_error("The {.strong model variable{?s}} {.var {variables}} must be present in {.arg data}"))
   }
-
-  if (!is.null(model$coefficients) & !all(names(model$coefficients) %in% colnames(data))) {
-    coef_not_in_data <- names(model$coefficients) %in% colnames(data)
-    coefs <- names(model$coefficients)[coef_not_in_data]
-    error_message <- c(error_message, "*" = cli::format_error("The {.strong model coefficient{?s}} {.var {coefs}} must be present in {.arg data}"))
-  }
-
-  # check that the means variables are in the `data` argument
-  if (methods::is(model, "cox") & is.null(model$means)) {
-    error_message <- c(error_message, cli::format_error(""))
-  }
-
-  if (methods::is(model, "cox") & !is.null(model$means) & !all(names(model$means) %in% colnames(data))) {
-    mean_not_in_data <- names(model$means) %in% colnames(data)
-    means <- names(model$means)[mean_not_in_data]
-    error_message <- c(error_message, "*" = cli::format_error("The {.strong model mean{?s}} {.var {means}} must be present in {.arg data}"))
-  }
-
 
   # check that the dataset is multiple imputed and contains an id
   if (!".imp" %in% colnames(data)) {
@@ -39,12 +22,6 @@ get_error_message_calculate <- function(model, data) {
 
   if (!"id" %in% colnames(data)) {
     error_message <- c(error_message, "*" = cli::format_error("The variable {.var id} (unique identifier for each row) must be present in {.arg data}"))
-  }
-
-  # if `model` is a logreg, check for its intercept
-  if (methods::is(model, "logreg") &
-    (!"intercept" %in% names(model) | !methods::is(model$intercept, "numeric"))) {
-    error_message <- c(error_message, "*" = "The {.strong model intercept} must be {.cls numeric} (check if exists)")
   }
 
   return(error_message)
