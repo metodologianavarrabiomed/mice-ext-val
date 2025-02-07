d0368c91b57b79bd96a9213ab5cb2dfc52cf1e3a
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2025-02-07 10:33:18 +0100

    first concept of the new print function

---
+++ R/print.MiceExtVal.R
@@ -0,0 +1,41 @@
+#' Title
+#'
+#' @param model
+#' @param ...
+#'
+#' @returns
+#' @exportS3Method print MiceExtVal
+#'
+#' @examples
+print.MiceExtVal <- function(x, ...) {
+  cli::cli_h1("{.cls {class(x)}}")
+
+  for (elem in names(x)) {
+    if (!is.null(x[[elem]])) {
+      cli::cli_h2(elem)
+
+      # show tibbles
+      if (methods::is(x[[elem]], "tbl_df")) {
+        print(head(x[[elem]], n = 5))
+      } else {
+        # show lists
+        if (methods::is(x[[elem]], "list")) {
+          list_text <- purrr::map2_chr(
+            names(x[[elem]]), x[[elem]], ~ cli::format_message("{.x} = {.val {.y}}")
+          )
+          cli::cli_text("{list_text}")
+        } else {
+          # c-index formatting
+          if (elem == "c_index") {
+            cli::cli_text(
+              "{.val {x[[elem]]['Estimate']}} (95% CI {.val {x[[elem]]['95% CI L']}}, {.val {x[[elem]]['95% CI U']}})"
+            )
+          } else {
+            # default
+            cli::cli_text(x[[elem]])
+          }
+        }
+      }
+    }
+  }
+}
