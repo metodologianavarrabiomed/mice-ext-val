2ccdd36edaba742aef13a27a087aa4f6093424fe
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2025-02-14 08:36:47 +0100

    add framingham dataset to the package

---
+++ R/framingham.R
@@ -0,0 +1,28 @@
+#' An example dataset from the Framingham study
+#'
+#' @format A data frame with nine variables:
+#' \describe{
+#' \item{\code{randid}}{id for each patient}
+#' \item{\code{period}}{period of the measure study}
+#' \item{\code{sex}}{sex of the patient}
+#' \item{\code{age}}{age of the patient}
+#' \item{\code{totchol}}{total cholesterol of the patient in mg/dL}
+#' \item{\code{hdlc}}{hdl of the patient in mg/dL}
+#' \item{\code{ldlc}}{ldl of the patient in mg/dL}
+#' \item{\code{sysbp}}{systolic blood pressure of the patient in mmHg}
+#' \item{\code{diabp}}{diastolic blood pressure of the patient in mmHg}
+#' \item{\code{cursmoke}}{if the patient is currently smoking}
+#' \item{\code{diabetes}}{if the patient is diagnosed with diabetes}
+#' \item{\code{anychd}}{if the patient have suffered any CHD event during follow-up}
+#' \item{\code{timechd}}{time of follow up of the patient}
+#' }
+#'
+#' @docType data
+#' @keywords dataset
+#' @name framingham
+#' @usage data(framingham)
+#' @format a dataset with 11627 rows and 13 variables
+#'
+#' For further details, see \url{https://www.kaggle.com/datasets/shreyjain601/framingham-heart-study}
+#'
+"framingham"
