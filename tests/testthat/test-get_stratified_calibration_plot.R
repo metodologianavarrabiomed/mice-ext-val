5e298bebcf08bdce9d0bf11dbc4a443ce5fc1dc5
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2025-03-18 08:59:21 +0100

    add tests for the new errors with not all models having predictions calculated

--- tests/testthat/test-get_stratified_calibration_plot.R
+++ tests/testthat/test-get_stratified_calibration_plot.R
@@ -49,6 +49,7 @@ test_that("generates an stratified calibration plot with recalibration type 2", 
     "ggplot"
   )
 })
+
 test_that("returns an error if some model is not of class <MiceExtVal>", {
   data <- readRDS(test_path("fixtures", "mice_data.rds"))
   model_cox <- make_cox_model(environment()) |>
@@ -58,11 +59,51 @@ test_that("returns an error if some model is not of class <MiceExtVal>", {
     calculate_predictions(data)
 
   testthat::expect_error(
-    get_stratified_calibration_plot(data = data, n_groups = 10, model_cox, 5),
+    get_stratified_calibration_plot(data = data, n_groups = 10, type = "predictions_aggregated", model_cox, 5),
     "must be <MiceExtVal>"
   )
   testthat::expect_error(
-    get_stratified_calibration_plot(data = data, n_groups = 10, model_logreg, 5),
+    get_stratified_calibration_plot(data = data, n_groups = 10, type = "predictions_aggregated", model_logreg, 5),
     "must be <MiceExtVal>"
   )
 })
+
+test_that("returns an error if no all the models have the predictions calculated", {
+  data <- readRDS(test_path("fixtures", "mice_data.rds"))
+  model_cox <- make_cox_model(environment())
+
+  model_logreg <- make_logreg_model(environment()) |>
+    calculate_predictions(data)
+testthat::expect_error(
+  get_stratified_calibration_plot(data = data, n_groups = 10, type = "predictions_aggregated", model_cox, model_logreg),
+  "must contain `predictions_aggregated` consider using the function"
+)
+  })
+
+test_that("returns an error if no all the models have the predictions recalibrated type 1 calculated", {
+  data <- readRDS(test_path("fixtures", "mice_data.rds"))
+  model_cox <- make_cox_model(environment()) |>
+    calculate_predictions(data) |>
+    calculate_predictions_recalibrated_type_1(data)
+
+  model_logreg <- make_logreg_model(environment()) |>
+    calculate_predictions(data)
+testthat::expect_error(
+  get_stratified_calibration_plot(data = data, n_groups = 10, type = "predictions_recal_type_1", model_cox, model_logreg),
+  "must contain `predictions_recal_type_1` consider using the function"
+)
+  })
+
+test_that("returns an error if no all the models have the predictions recalibrated type 1 calculated", {
+  data <- readRDS(test_path("fixtures", "mice_data.rds"))
+  model_cox <- make_cox_model(environment()) |>
+    calculate_predictions(data) |>
+    calculate_predictions_recalibrated_type_2(data)
+
+  model_logreg <- make_logreg_model(environment()) |>
+    calculate_predictions(data)
+testthat::expect_error(
+  get_stratified_calibration_plot(data = data, n_groups = 10, type = "predictions_recal_type_2", model_cox, model_logreg),
+  "must contain `predictions_recal_type_2` consider using the function"
+)
+  })
