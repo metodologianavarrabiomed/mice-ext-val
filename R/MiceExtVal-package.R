d682a77594fd10ae2ddd07c20069f7b8dd4e1c3f
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2025-04-01 13:56:42 +0200

    update general documentation of the package

---
+++ R/MiceExtVal-package.R
@@ -0,0 +1,14 @@
+#' MiceExtVal
+#'
+#' @description
+#' This package allows the user to externally validate Cox and Logistic Regression models from the data that are usually reported in the articles. The package is formed by functions that allow the user to recalibrate the models and also obtain the discrimination and calibration performance while working with multiple imputed data.
+#'
+#' To learn more about MiceExtVal, we recommend you to start from the webpage article
+#'
+#' <https://metodologianavarrabiomed.github.io/mice-ext-val/articles/get-started.html>
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
