e0f627992afc0ae1aad7dc3473d43ada356ce1be
Asier Ballesteros Dominguez <asierolvega@gmail.com>
2025-05-20 12:30:49 +0200

    update print function to also display the brier score

--- R/print.MiceExtVal.R
+++ R/print.MiceExtVal.R
@@ -38,7 +38,7 @@ print.MiceExtVal <- function(x, ...) {
           cli::cli_text("{list_text}")
         } else {
           # c-index formatting
-          if (elem %in% c("c_index", "auc")) {
+          if (elem %in% c("c_index", "auc", "brier_score")) {
             cli::cli_text(
               "{.val {x[[elem]]['Estimate']}} (95% CI {.val {x[[elem]]['95% CI L']}}, {.val {x[[elem]]['95% CI U']}})"
             )
